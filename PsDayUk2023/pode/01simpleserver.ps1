3edc7ef90f2b9267a0ff0ff8fbb9af6adb3cedd5
Kamil Pro <kprocyszyn@gmail.com>
2023-02-05 14:35:42 +0000

    Add frontend

--- PsDayUk2023/pode/01simpleserver.ps1
+++ PsDayUk2023/pode/01simpleserver.ps1
@@ -1,4 +1,6 @@
 Import-Module -Name (Join-Path $PSScriptRoot "lib.ps1") -ErrorAction Stop
+Import-Module -Name (Join-Path $PSScriptRoot "pode/Pode/2.7.2/Pode.psd1") -ErrorAction Stop
+Import-Module -Name (Join-Path $PSScriptRoot "pode/Pode.Web/0.8.2/Pode.Web.psd1") -ErrorAction Stop
 
 Start-PodeServer {
     Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http
@@ -20,4 +22,21 @@ Start-PodeServer {
 
        Write-PodeJsonResponse -Value (Get-User @params)
     }
+
+    Add-PodeRoute -Method Get -Path '/users/:userId' -ScriptBlock {
+
+       Write-PodeJsonResponse -Value (Get-User -Id $WebEvent.Parameters['userId'])
+    }
+
+    Use-PodeWebTemplates -Title 'PsDayUk 2023' -Theme Light
+
+    $navDiv = New-PodeWebNavDivider
+    $navPode = New-PodeWebNavLink -Name 'Pode' -Url 'https://badgerati.github.io/Pode/' -Icon 'server' -NewTab
+    $navPodeWeb = New-PodeWebNavLink -Name 'PodeWeb' -Url 'https://badgerati.github.io/Pode.Web/' -Icon 'web-check' -NewTab
+    $navYT = New-PodeWebNavLink -Name 'YouTube' -Url 'https://www.youtube.com/c/KamilPro' -Icon 'youtube' -NewTab
+    $navGH = New-PodeWebNavLink -name 'GitHub' -Url 'https://github.com/kprocyszyn/About-PowerShell' -Icon 'github' -NewTab
+    $navPwpush = New-PodeWebNavLink -name 'PwPush' -Url 'https://pwpush.com/' -Icon 'lock-check' -NewTab
+    Set-PodeWebNavDefault -Items $navPode, $navDiv, $navPodeWeb, $navDiv, $navYT, $navDiv, $navGH, $navDiv, $navPwpush
+
+    Use-PodeWebPages
 }
