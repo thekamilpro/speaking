e86fb5be6552c213be716cea001b075c017154b0
Kamil Pro <kprocyszyn@gmail.com>
2023-02-06 20:33:40 +0000

    Add chartprocess

---
+++ PsDayUk2023/pode/pages/chartprocess.ps1
@@ -0,0 +1,13 @@
+Add-PodeWebPage -Name "Chart - Processes" -ScriptBlock {
+
+    New-PodeWebContainer -Content @(
+    New-PodeWebChart -Name 'Top Processes' -Type Bar -AutoRefresh -ScriptBlock {
+        Get-Process |
+            Sort-Object -Property CPU -Descending |
+            Select-Object -First 10 |
+            Select-Object ProcessName,CPU,@{Name='MemoryGB';Expression={($_.WorkingSet64/1GB)}} |
+            ConvertTo-PodeWebChartData -LabelProperty ProcessName -DatasetProperty CPU, MemoryGB
+    }
+)
+
+}
