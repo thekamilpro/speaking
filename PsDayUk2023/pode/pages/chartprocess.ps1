e9bce9e34d651c092337a60d02c23af15679de38
Kamil Pro <kprocyszyn@gmail.com>
2023-02-08 20:26:35 +0000

    add survey page

--- PsDayUk2023/pode/pages/chartprocess.ps1
+++ PsDayUk2023/pode/pages/chartprocess.ps1
@@ -1,13 +1,12 @@
 Add-PodeWebPage -Name "Chart - Processes" -ScriptBlock {
 
     New-PodeWebContainer -Content @(
-    New-PodeWebChart -Name 'Top Processes' -Type Bar -AutoRefresh -ScriptBlock {
-        Get-Process |
+        New-PodeWebChart -Name 'Top Processes' -Type Bar -AutoRefresh -ScriptBlock {
+            Get-Process |
             Sort-Object -Property CPU -Descending |
             Select-Object -First 10 |
-            Select-Object ProcessName,CPU,@{Name='MemoryGB';Expression={($_.WorkingSet64/1GB)}} |
+            Select-Object ProcessName, CPU, @{Name = 'MemoryGB'; Expression = { ($_.WorkingSet64 / 1GB) } } |
             ConvertTo-PodeWebChartData -LabelProperty ProcessName -DatasetProperty CPU, MemoryGB
-    }
-)
-
+        }
+    )
 }
---
+++ PsDayUk2023/pode/pages/survey.ps1
@@ -0,0 +1,36 @@
+Add-PodeWebPage -Name 'Survey' -DisplayName "Survey" -ScriptBlock {
+
+    New-PodeWebHeader -Size 3 -Value "Hello Bob!"
+
+    New-PodeWebForm -Name "Find user" -Content @(
+        New-PodeWebHidden -Name "upn" -Value "bob@company.pri"
+        New-PodeWebRadio -Name "attend" -DisplayOptions @("Yes and enjoyed", "No but still enjoyed") -Options @("yes", "no")
+        New-PodeWebCheckbox -DisplayName "Ok to Contact?" -Name "contact" -Options @("yes") -AsSwitch
+        New-PodeWebTextbox -Name "comments" -Placeholder "Please add any other comments" -MaxLength 20
+    ) -ScriptBlock {
+        $WebEvent.Data | Out-Default
+
+        Update-PodeWebTextbox -Value $WebEvent.Data.upn -Name "Email"
+        Update-PodeWebTextbox -Value $WebEvent.Data.attend -Name "Attended"
+
+        Clear-PodeWebTextbox -Name contact
+        if (![String]::IsNullOrEmpty($WebEvent.Data.contact))
+        {
+            Update-PodeWebTextbox -Value $WebEvent.Data.contact -Name "contact"
+        }
+        Clear-PodeWebTextbox -Name Comments
+        if (![String]::IsNullOrEmpty($WebEvent.Data.Comments))
+        {
+            Update-PodeWebTextbox -Value $WebEvent.Data.comments -Name "Comments"
+        }
+
+        Update-PodeWebTextbox -Name multi -Value ($WebEvent.Data | Out-String) -Multiline
+    }
+    
+    New-PodeWebTextbox -Name Email -ReadOnly
+    New-PodeWebTextbox -Name Attended -ReadOnly
+    New-PodeWebTextbox -Name contact -ReadOnly
+    New-PodeWebTextbox -Name Comments -ReadOnly    
+
+    New-PodeWebTextbox -Name multi -Multiline
+}
