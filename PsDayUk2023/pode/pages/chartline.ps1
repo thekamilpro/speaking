09ab7b3cb78401ea43f664e3981b787983216387
Kamil Pro <kprocyszyn@gmail.com>
2023-02-08 20:48:29 +0000

    add chart line

---
+++ PsDayUk2023/pode/pages/chartline.ps1
@@ -0,0 +1,20 @@
+Add-PodeWebPage -Name "Chart - Line" -ScriptBlock {
+
+    New-PodeWebChart -Name 'Example Chart' -Type Line -ScriptBlock {
+        return (1..10 | ForEach-Object {
+            @{
+                Key = $_ # x-axis value
+                Values = @(
+                    @{
+                        Key = 'Group1'
+                        Value = (Get-Random -Maximum 10) # y-axis value
+                    }
+                    @{
+                        Key = 'Group2'
+                        Value = (Get-Random -Maximum 10) # y-axis value
+                    }
+                )
+            }
+        })
+    }
+}
