6e286d479ad35ec42739d73635f52a7204483826
Kamil Pro <kprocyszyn@gmail.com>
2023-02-12 15:14:40 +0000

    Expand advanced survey

--- PsDayUk2023/pode/pages/surveyadvanced.ps1
+++ PsDayUk2023/pode/pages/surveyadvanced.ps1
@@ -1,40 +1,77 @@
-Add-PodeWebPage -Name "Survey Advanced" -ScriptBlock {
-
-    $computers = @{
+function Get-Computers {
+    return @{
         machine1 = @(
-            [pscustomobject]@{id = 1; software = "Firefox 23"; reviewed = $true; action = "upgrade"},
-            [pscustomobject]@{id = 2; software = "Java 3"; reviewed = $false; action = "uninstall"},
-            [pscustomobject]@{id = 3; software ="Internet Explorer 7"; reviewed = $false; action = "uninstall"}
+            [pscustomobject]@{id = 1; software = "Firefox 23"; reviewed = $true; action = "upgrade" },
+            [pscustomobject]@{id = 2; software = "Java 3"; reviewed = $false; action = "uninstall" },
+            [pscustomobject]@{id = 3; software = "Internet Explorer 7"; reviewed = $false; action = "uninstall" }
         )
 
         machine2 = @(
-            [pscustomobject]@{id = 1; software = "Winzip 3"; reviewed = $false; action = "uninstall"},
-            [pscustomobject]@{id = 2; software = "Java 5"; reviewed = $false; action = "uninstall"}
+            [pscustomobject]@{id = 1; software = "Winzip 3"; reviewed = $false; action = "uninstall" },
+            [pscustomobject]@{id = 2; software = "Java 5"; reviewed = $false; action = "uninstall" }
         )
     }
+}
 
-    foreach ($c in $computers.Keys)
-    {
-       New-PodeWebCard -Name $c -Content @(
-            New-PodeWebTable -Name "Software $c" -ArgumentList (,$computers[$c]) -ScriptBlock {
+Add-PodeWebPage -Name "Survey Advanced" -ScriptBlock {
+
+    $computers = Get-Computers
+
+    foreach ($c in $computers.Keys) {
+        New-PodeWebCard -Name $c -Content @(
+            New-PodeWebTable -Name "Software $c" -ArgumentList ($c, $computers[$c]) -DataColumn "Payload" -ScriptBlock {
                 param (
-                    $software
+                    $ComputerName,
+                    $Software
                 )
 
-                foreach ($s in $software)
-                {
-                    if ($s.reviewed) {$colour = "green"; $text = "Yes"} else {$colour = "red"; $text = "No"}
+                foreach ($s in $Software) {
+                    if ($s.reviewed) { $colour = "green"; $text = "Yes" } else { $colour = "red"; $text = "No" }
 
                     [ordered]@{
-                        ID = $s.id
+                        ID       = $s.id
                         Software = $s.software
                         Reviewed = New-PodeWebBadge -Colour $colour -Value $text
-                        Action = $s.action
-                        Edit = New-PodeWebButton -Name "Edit" -ScriptBlock {}
+                        Action   = $s.action
+                        Edit     = New-PodeWebButton -Name "Edit" -ScriptBlock { Move-PodeWebPage -Name "Survey-Advanced-Edit" -DataValue $Webevent.Data['Value'] }
+                        Payload  = "$($ComputerName)|$($s.Id)"
                     }
                 }
             }
-       )
+        )
+    }
+}
+
+Add-PodeWebPage -Hide -NoSidebar -Name "Survey-Advanced-Edit" -DisplayName "Survey Advanced Edit"  -ScriptBlock {
+
+    if (!$WebEvent.Query['Value']) {
+        New-PodeWebHeader -Value "You need to pass in information" -Size 3
+        return
     }
 
+    $data = $WebEvent.Query['Value'].split('|')
+    $computerName = $data[0].trim()
+    $softwareId = $data[1].trim()
+
+    $computers = Get-Computers
+    $computer = $computers[$computerName]
+    $software = $computer | Where-Object { $_.Id -eq $softwareId }
+
+    if ($null -eq $software) {
+        New-PodeWebHeader -Value "Failed to find info $data; $computerName; $softwareId" -Size 3
+        return
+    }
+
+    New-PodeWebContainer -Content @(
+        New-PodeWebForm -Name "Edit" -Content @(
+        
+            New-PodeWebTextbox -Name id -Value $software.id -ReadOnly
+            New-PodeWebTextbox -name software -value $software.software -ReadOnly 
+            New-PodeWebTextbox -name reviewed -value $true -ReadOnly
+            New-PodeWebRadio -Name "Action" -Options @("Upgrade", "Uninstall")
+            New-PodeWebTextbox -Name "comment" -Multiline   
+        ) -ScriptBlock {
+            Show-PodeWebToast -Title "Success" -Message "Answer saved"
+        }
+    )
 }
